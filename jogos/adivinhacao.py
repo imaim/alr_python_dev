a7181b71130cde79c5cd16c3896a00d11762d386
imaim vilela <imaim.filho@gmail.com>
2023-03-02 13:43:19 -0300

    numero randomico

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,13 +1,31 @@
+import random as rd
+
+
+
 print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
 
-numero_secreto = 31
+def randomico():
+    laco = 0
+    numero_secreto = 0
+    while laco >= 0:
+        numero_secreto = round(rd.random() * 101)
+        if 0 < numero_secreto <= 100:
+            #print(numero_secreto)
+            return numero_secreto
+            break
+        else:
+            #print(numero_secreto)
+            continue
+        laco += 1
 
 
 def numero(tent):
 
+    #numero_secreto = randomico()
+    numero_secreto = round(rd.randrange(1, 101))
     total_tentativas = tent
 
     for rodada in range(1, total_tentativas + 1):
@@ -24,15 +42,29 @@ def numero(tent):
             continue
 
         if errou:
-            if maior:
-                print(f"Tentativa {rodada} de {total_tentativas}\n"
-                      f"Nao acertou, seu palpite foi Maior que o numero secreto \n"
-                      f"Seu Ultimo Palpite foi {palpite}")
-            elif menor:
-                print(f"Tentativa {rodada} de {total_tentativas}\n"
-                      f"Nao acertou, seu palpite foi Menor que o numero secreto \n" 
-                      f"Seu Ultimo Palpite foi {palpite}")
+
+            if rodada == total_tentativas:
+
+                if maior:
+                    print(f"Tentativa {rodada} de {total_tentativas}\n"
+                          f"Nao acertou, seu palpite foi Maior que o numero secreto \n"
+                          f"Seu Ultimo Palpite foi {palpite} e o Número Secreto era {numero_secreto}")
+                elif menor:
+                    print(f"Tentativa {rodada} de {total_tentativas}\n"
+                          f"Nao acertou, seu palpite foi Menor que o numero secreto \n" 
+                          f"Seu Ultimo Palpite foi {palpite} e o Número Secreto era {numero_secreto}")
+            else:
+                if maior:
+                    print(f"Tentativa {rodada} de {total_tentativas}\n"
+                          f"Nao acertou, seu palpite foi Maior que o numero secreto \n"
+                          f"Seu Ultimo Palpite foi {palpite}")
+                elif menor:
+                    print(f"Tentativa {rodada} de {total_tentativas}\n"
+                          f"Nao acertou, seu palpite foi Menor que o numero secreto \n" 
+                          f"Seu Ultimo Palpite foi {palpite}")
+
         else:
+
             print(f"Tentativa {rodada} de {total_tentativas}\n"
                   f"Acertou o Número Secreto {numero_secreto} !")
             break
@@ -40,5 +72,5 @@ def numero(tent):
         rodada += 1
 
 
-numero(5)
+numero(3)
 print(f"Fim do Jogo")
