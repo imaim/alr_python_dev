80c975dad41007ff07d691cbc72b2216b7be41cf
imaim.vilela <imaim.filho@gmail.com>
2023-03-01 21:16:10 -0300

    laços de repetição ehile

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -2,17 +2,38 @@ print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
-numero_secreto = 42
 
-palpite = input("Qual seu palpite para o numero Secreto ?")
-
-
-class NumeroTest:
-    def __init__(self, numero):
-        self.valor = numero
-        while self.valor != numero_secreto:
-            return print("Voce não acertou")
-        return print("Voce acertou")
+numero_secreto = 31
 
 
-print(NumeroTest(palpite))
+def numero(tent):
+
+    total_tentativas = tent
+    rodada = 1
+
+    while 0 < rodada <= total_tentativas:
+
+        palpite = int(input("Qual seu palpite para o numero Secreto ? "))
+        errou = (palpite != numero_secreto)
+        maior = (palpite > numero_secreto)
+        menor = (palpite < numero_secreto)
+
+        if errou:
+            if maior:
+                print(f"Tentativa {rodada} de {total_tentativas}\n"
+                      f"Nao acertou, seu palpite foi Maior que o numero secreto \n"
+                      f"Seu Ultimo Palpite foi {palpite}")
+            elif menor:
+                print(f"Tentativa {rodada} de {total_tentativas}\n"
+                      f"Nao acertou, seu palpite foi Menor que o numero secreto \n" 
+                      f"Seu Ultimo Palpite foi {palpite}")
+        else:
+            print(f"Tentativa {rodada} de {total_tentativas}\n"
+                  f"Acertou o Número Secreto {numero_secreto} !")
+            break
+
+        rodada += 1
+
+
+numero(5)
+print(f"Fim do Jogo")
