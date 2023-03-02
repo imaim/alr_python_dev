cf4b8c7664dfb8fe50895db7dfd67730f9ae3ffd
Imaim Vilela <imaim.vilela@2comconsulting.com.br>
2023-03-02 10:00:02 -0300

    teste push

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
 
 curl "http://{{ansible_host}}/master/check_mk/webapi.py?_secret={{vault_ansible_automation}}&_username={{auto_user}}&action=activate_changes" -d 'request={"sites":["{{omd_site}}"],"allow_foreign_changes":"1"}'
 print("teste")
-print("2")
+print("23")
