0228b16c937687aa983989dede30b2fabf4d4c20
imaim vilela <imaim.filho@gmail.com>
2023-03-03 22:36:55 -0300

    jogo forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,20 @@ def jogo_forca():
     print("Bem vindo ao jogo de Forca")
     print("********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        print(f"iniciando Jogo")
+
+        chute = str.lower(input("Digite uma letra "))
+        index = 0
+        for letra in palavra_secreta:
+            if letra in chute:
+                print(f"{chute} encontrada na posição {index}")
+            index += 1
+
 
 if __name__ == "__main__":
     jogo_forca()
