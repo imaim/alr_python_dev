5fa22ae6ff3a4ce628127930148aeecf947485ca
imaim vilela <imaim.filho@gmail.com>
2023-03-06 22:20:16 -0300

    Orientacao objeto iniciando - conceito

---
+++ orientacao_objeto/teste.py
@@ -0,0 +1,19 @@
+
+
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+
+def depositar(conta, valor):
+    conta["saldo"] += valor
+
+
+def sacar(conta, valor):
+    conta["saldo"] -= valor
+
+
+def extrato(conta):
+    print(f"Seu saldo é {conta['saldo']}")
+
+
