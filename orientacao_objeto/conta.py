b6ab7a3ee4d313d1fbc68ef498ea28abc4e481b9
imaim.filho <imaim.filho@gmail.com>
2023-03-09 09:34:12 -0300

    orientacao objeto - metodos, classes e atributos, desafio datas

--- orientacao_objeto/conta.py
+++ orientacao_objeto/conta.py
@@ -1,8 +1,19 @@
 
-class ContaCorrente:
+class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo Objeto.... {self}")
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limit = limite
+
+    def extrato(self):
+        print(f"Saldo {self.saldo} do Titular {self.titular}")
+
+    def saca(self, valor):
+        self.saldo -= valor
+        print(f"Saldo {self.saldo} do Titular {self.titular}")
+
+    def deposita(self, valor):
+        self.saldo += valor
+        print(f"Saldo {self.saldo} do Titular {self.titular}")
