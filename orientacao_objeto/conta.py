70ff90cedf03a9f2528e635dff0912d82da19254
imaim vilela <imaim.filho@gmail.com>
2023-03-08 22:26:13 -0300

    OO - classe e atributos, referencias dos objetos na memoria

---
+++ orientacao_objeto/conta.py
@@ -0,0 +1,8 @@
+
+class ContaCorrente:
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Construindo Objeto.... {self}")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limit = limite
