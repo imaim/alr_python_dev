ab00dfb2075fc39e5053ceab371a1fe731c8dd88
imaim vilela <imaim.filho@gmail.com>
2023-03-21 22:14:15 -0300

    orientacao objeto- coleta e tratamento de url

--- estrator_url/main.py
+++ estrator_url/main.py
@@ -1,5 +1,6 @@
 # "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-url = "bytebank.com/cambio?moedaOrigem=real"
-url_base = url[0:19]
-url_parametro = url[20:36]
+url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametro = url[indice_interrogacao+1:]
 print(url_parametro)
