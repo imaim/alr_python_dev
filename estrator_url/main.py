af19857127402b8aea1a10596c432d813ba6ad03
imaim vilela <imaim.filho@gmail.com>
2023-03-21 23:04:11 -0300

    orientacao objeto- coleta e tratamento de url - split

--- estrator_url/main.py
+++ estrator_url/main.py
@@ -3,4 +3,16 @@ url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
 url_parametro = url[indice_interrogacao+1:]
-print(url_parametro)
+
+parametro_busca = "moedaDestino"
+indice_parametro = url_parametro.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+teste = url_parametro.split(sep='&')
+teste_dict = {}
+count = 0
+for i in teste:
+    teste_dict[i.split(sep="=")[0]] = i.split(sep="=")[1]
+    count += 1
+valor = url_parametro[indice_valor:]
+print(valor)
+print(teste_dict['moedaOrigem'])
